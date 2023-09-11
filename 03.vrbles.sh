45b7107f928aab5942ee581d5ffe930dff65f475
Yalaga surendra babu <yalagasurendrababu6@gmail.com>
2023-09-11 10:22:00 +0530

    execute scripting backend

---
+++ 03.vrbles.sh
@@ -0,0 +1,16 @@
+#if we give name some data we will that variable
+
+#syntax delcar
+#var_name = data
+
+a=10
+x=100
+y=150
+z=true
+name=devops
+
+#those are all var delaration
+
+#how to access variables
+
+echo value of a = $a
