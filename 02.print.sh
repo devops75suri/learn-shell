a82c2c74bda0425cdcac47e2c8705d32f58c8f60
Yalaga surendra babu <yalagasurendrababu6@gmail.com>
2023-09-11 14:52:15 +0530

    execute scripting backend

--- 02.print.sh
+++ 02.print.sh
@@ -1,1 +1,26 @@
 echo hai surendra how is going clas
+
+# there are 6 colores in shell
+
+# 1.red  - 31
+# 2.Green -32
+# 3.yellow -33
+# 4.blue  -34
+# 5.magenta -35
+# 6.cyan -36
+
+#syntax of color print
+# echo -e "\e[COLmMESSAGE\[0m"
+# -e option to enable color
+# \e[COLM - to defined which color
+# \e[0m - ti disable the color or end the color
+
+
+echo -e "\e[31mRED COLOR\e[0m"
+echo -e "\e[32mGREEN COLOR\e[0m"
+echo -e "\e[33mYELLOW COLOR\e[0m"
+echo -e "\e[34mBLUE COLOR\e[0m"
+echo -e "\e[35mMAGEMTA COLOR\e[0m"
+echo -e "\e[36mCYAN COLOR\e[0m"
+
+
